a53a88ea60e977a2e8b6015075483c0c2fbee9e3
Cỏ Khô <92889822+HuynhCoKho@users.noreply.github.com>
2026-06-30 21:27:59 +0700

    Add config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+window.FLASHCARD_CONFIG = {
+  APPS_SCRIPT_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbxIsOzuwBKKHhA1MqV7sJSNh-NE8WkRGUBTf6XSBV7_XVi25aysXbNymbbC02KB0Hmh/exec',
+  SPREADSHEET_ID: '1MDt7HLzuhndU30Ln2uW7Ibd9O1M2FRqTtpGw3dRw_wM',
+  POINTS_CORRECT: 10,
+  POINTS_WRONG: -4,
+  RECENT_WINDOW: 6,
+  DEFAULT_PLAYER_NAME: 'Người học'
+};
